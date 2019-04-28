776b9420102ff1fa58baed7d34662900adbee5e4
zhangtao <jaychou714642403@aliyun.com>
2019-04-28 16:50:21 +0800

    提交pom文件以及测试方法。

--- src/main/java/com/java/zhang/JavaNotes/JavaNotesApplication.java
+++ src/main/java/com/java/zhang/JavaNotes/JavaNotesApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaNotesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JavaNotesApplication.class, args);
+		System.out.println("启动成功!");
 	}
 
 }
